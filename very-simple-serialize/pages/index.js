0e238fbb5d46a536d5b5b3d7934d8329caf05b8d
ScottyPoi <kyzambo@gmail.com>
2021-06-17 23:42:12 -0600

    welcome page

--- very-simple-serialize/pages/index.js
+++ very-simple-serialize/pages/index.js
@@ -13,54 +13,31 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          SSZ Visualizer
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Explore SSZ Types and Merkle Trees
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <button className={`btn btn-primary`} style={{border: 'solid black'}}>
+          <h2>
+            START
+          </h2>
+        </button>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <div className='p-4'><h4>Learn more about SSZ SimpleSerialize at <a  href='https://www.ssz.dev'>www.ssz.dev</a></h4></div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://esp.ethereum.foundation/en/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src='/horz-logo-065bcbe38b2208c0239b3bc7c661258e.svg' alt="ESP Logo" width={200} height={100} />
           </span>
         </a>
       </footer>
